5581bdd7343744a31786207afdf72ac1bb2cfe4e
Connor Bratten <connorabratten@gmail.com>
2016-12-28 09:20:00 -0500

    Add new URL to add item to existing list

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -18,6 +18,7 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
+        sleeptime = 1
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
         self.browser.get(self.live_server_url)
@@ -39,7 +40,7 @@ class NewVisitorTest(LiveServerTestCase):
         # and now the page lists "1: Buy peacock feathers" as an item in a
         # to-do list table
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(2)
+        time.sleep(sleeptime)
         edith_list_url = self.browser.current_url
         self.assertRegex(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
@@ -50,7 +51,7 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(2)
+        time.sleep(sleeptime)
 
         # The page updates again, and now shows both items on her list
         table = self.browser.find_element_by_id('id_list_table')
@@ -76,7 +77,7 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(2)
+        time.sleep(sleeptime)
 
         # Francis gets his own unique URL
         francis_list_url = self.browser.current_url
@@ -84,7 +85,7 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotEqual(francis_list_url, edith_list_url)
 
         # Again, there is no trace of Edith's list
-        page_text = self.browserl.find_element_by_tag_name('body').text
+        page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
 
